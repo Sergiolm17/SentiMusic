fc43489b9dee68908ab08824398f39ffef5cbc6e
Sergiolm17 <kingsergiolm17@gmail.com>
2019-09-04 17:47:46 +0000

    scopes

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get("/login", function(req, res) {
 
   // your application requests authorization
   var scope =
-    "user-read-private user-read-email user-read-playback-state playlist-modify-private user-modify-playback-state";
+    "user-read-private user-read-email user-read-playback-state playlist-modify-private playlist-modify-public user-modify-playback-state";
   res.redirect(
     "https://accounts.spotify.com/authorize?" +
       querystring.stringify({
