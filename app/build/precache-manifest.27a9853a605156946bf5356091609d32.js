7dab8ad3bc1761dc2b31e911cb60e7e99772d07c
Sergiolm17 <kingsergiolm17@gmail.com>
2019-09-04 15:09:00 +0000

    app

---
+++ app/build/precache-manifest.27a9853a605156946bf5356091609d32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4de679635bcc975bbd57bcdbc8358c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f883078f5a091633d26",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "60516f955e0f937dfea8",
+    "url": "/static/js/2.7ab406fd.chunk.js"
+  },
+  {
+    "revision": "2f883078f5a091633d26",
+    "url": "/static/js/main.3d24d856.chunk.js"
+  },
+  {
+    "revision": "1246f5376b78f8c5fbeb",
+    "url": "/static/js/runtime~main.804b5e3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
