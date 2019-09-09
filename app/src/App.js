b210c722a1e13d259015d22b23ec6715f3422cf4
Sergiolm17 <kingsergiolm17@gmail.com>
2019-09-09 05:08:23 +0000

    hook and app

--- app/src/App.js
+++ app/src/App.js
@@ -1,134 +1,100 @@
-import React, { useState, useEffect } from "react";
-import SpotifyWebApi from "spotify-web-api-js";
-const spotifyApi = new SpotifyWebApi();
+import React from "react";
+
+import {
+  useAccessToken,
+  useGetMe,
+  useGetNowPlaying,
+  useGetDevice,
+  useRecomendation,
+  useGetAudio /*
+  ,
+  getRecomendation,
+  getAudio,
+  getSearch,
+  createPlaylist,
+  addToPlaylist,
+  removeToPlaylist*/
+} from "./hooks/User";
+
+const urllocal = "http://34.68.6.184:4001";
+const urlprod = "http://34.68.6.184:4001";
+let appurl =
+  process.env.NODE_ENV === "production" ? urlprod : urllocal + "/login";
 
 function App() {
-  const params = getHashParams();
-  const [loggedIn, setloggedIn] = useState(false);
-  const [devices, setdevices] = useState([]);
-  const [me, setme] = useState({});
-
-  const [nowPlaying, setnowPlaying] = useState({
-    name: "",
-    albumArt: "",
-    is_playing: false,
-    uri: "",
-    id: ""
-  });
-
-  useEffect(() => {
-    const token = params.access_token;
-
-    if (token) {
-      spotifyApi.setAccessToken(token);
-      getMe();
-    }
-
-    setloggedIn(token ? true : false);
-  }, [params.access_token]);
-  const getNowPlaying = () => {
-    spotifyApi.getMyCurrentPlaybackState().then(response => {
-      console.log(response);
-      if (response)
-        setnowPlaying({
-          name: response.item.name,
-          albumArt: response.item.album.images[0].url,
-          is_playing: response.is_playing,
-          uri: response.item.uri,
-          id: response.item.id
-        });
-    });
-  };
-  const getMe = () => {
-    spotifyApi.getMe().then(user => {
-      console.log(user);
-      setme(user);
-    });
-  };
-  const getDevice = () => {
-    spotifyApi.getMyDevices().then(device => {
-      console.log(device);
-
-      setdevices(device.devices);
-    });
-  };
-  const getRecomendation = () => {
-    if (nowPlaying.id)
-      spotifyApi
-        .getRecommendations({
-          limit: 10,
-          market: "PE",
-          //seed_artists: "4NHQUGzhtTLFvgF5SZesLK",
-          seed_tracks: nowPlaying.id,
-          min_energy: 0.4,
-          min_popularity: 50
-        })
-        .then(device => {
-          console.log(device);
-        });
-  };
-  const getAudio = () => {
-    if (nowPlaying.id)
-      spotifyApi.getAudioFeaturesForTrack(nowPlaying.id).then(device => {
-        console.log(device);
-      });
-  };
-  const getSearch = () => {
-    spotifyApi
-      .search("WONDERLAND", ["track"], { market: "PE" })
-      .then(device => {
-        console.log(device);
-      });
-  };
-  const createPlaylist = () => {
-    spotifyApi
-      .createPlaylist(me.id, {
-        name: "Creado desde el server",
-        public: false,
-        description: "esta lpaylist es la recomendada"
-      })
-      .then(device => {
-        console.log(device);
-      });
-  };
-  const addToPlaylist = () => {
-    if (nowPlaying.uri)
-      spotifyApi
-        .addTracksToPlaylist("06sL90oZx5lI8mbaGzlgFD", [nowPlaying.uri])
-        .then(device => {
-          console.log(device);
-        });
-  };
-  const removeToPlaylist = () => {
-    if (nowPlaying.uri)
-      spotifyApi
-        .removeTracksFromPlaylist("06sL90oZx5lI8mbaGzlgFD", [nowPlaying.uri])
-        .then(device => {
-          console.log(device);
-        });
-  };
+  const loggedIn = useAccessToken();
+  const [me] = useGetMe();
+  const [nowPlaying, current] = useGetNowPlaying();
+  const [devices] = useGetDevice(nowPlaying);
+  const [audiodetail] = useGetAudio(nowPlaying);
+  const [recomendation] = useRecomendation(nowPlaying);
 
   return (
     <div>
-      {!loggedIn && (
-        <a href="https://sentimusic.herokuapp.com/login"> Login to Spotify </a>
-      )}
+      {!loggedIn && <a href={appurl}> Login to Spotify </a>}
       {me.display_name && "hola " + me.display_name}
-
-      {nowPlaying && (
+      {nowPlaying.name && current && (
         <div>
-          Ahora reproduciendo: {nowPlaying.name}
+          <p>Ahora reproduciendo: {nowPlaying.name}</p>
           <img
             alt={nowPlaying.name}
             src={nowPlaying.albumArt}
-            style={{ height: 150 }}
+            style={{ height: 300 }}
           />
         </div>
       )}
+      <p>
+        {devices.length > 0 &&
+          "se encontro " + devices.length + " dispositivos"}
+        {devices.map(device => (
+          <li key={device.id}>
+            {device.name} {device.tylie}
+          </li>
+        ))}
+      </p>
+      <div>
+        acousticness-{audiodetail.acousticness}
+        <p>danceability- {audiodetail.danceability}</p>
+        <p>duration_ms- {audiodetail.duration_ms}</p>
+        <p>energy- {audiodetail.energy}</p>
+        <p>instrumentalness- {audiodetail.instrumentalness}</p>
+        <p>key- {audiodetail.key}</p>
+        <p>liveness- {audiodetail.liveness}</p>
+        <p>loudness- {audiodetail.loudness}</p>
+        <p>duration_ms- {audiodetail.duration_ms}</p>
+        <p>mode- {audiodetail.mode}</p>
+        <p>speechiness- {audiodetail.speechiness}</p>
+        <p>tempo- {audiodetail.tempo}</p>
+        <p>time_signature- {audiodetail.time_signature}</p>
+        <p>valence- {audiodetail.valence}</p>
+      </div>
+      Recomendaciones
+      {recomendation.map((music, indexaudio) => (
+        <div key={music.id}>
+          <p>
+            {music.artists[0].name}-{music.name}
+          </p>
+          <img
+            alt={music.name}
+            src={music.album.images[0].url}
+            style={{ height: 100 }}
+          />
+          <audio controls src={music.preview_url} preload="none">
+            Your browser does not support the
+            <code>audio</code> element.
+          </audio>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
+/*
+
+     
       {loggedIn && (
         <>
-          <button onClick={() => getNowPlaying()}>Ahora reproduciendo</button>
-          <button onClick={() => getDevice()}>Get Device</button>
           <button onClick={() => getSearch()}>Search</button>
           <button onClick={() => createPlaylist()}>createPlaylist</button>
         </>
@@ -141,29 +107,10 @@ function App() {
           <button onClick={() => removeToPlaylist()}>removeToPlaylist</button>
         </>
       )}
-      <p>
-        {devices.length > 0 &&
-          "se encontro " + devices.length + " dispositivos"}
-      </p>
+     
       {devices.map(device => (
         <p key={device.id}>
           {device.name} {device.type}
         </p>
       ))}
-    </div>
-  );
-}
-const getHashParams = () => {
-  var hashParams = {};
-  var e,
-    r = /([^&;=]+)=?([^&;]*)/g,
-    q = window.location.hash.substring(1);
-  e = r.exec(q);
-  while (e) {
-    hashParams[e[1]] = decodeURIComponent(e[2]);
-    e = r.exec(q);
-  }
-  return hashParams;
-};
-
-export default App;
+*/
---
+++ app/src/hooks/User.js
@@ -0,0 +1,171 @@
+import { useState, useEffect } from "react";
+
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi();
+
+function useAccessToken() {
+  const params = getHashParams();
+
+  const [loggedIn, setloggedIn] = useState(false);
+
+  useEffect(() => {
+    if (params.access_token) {
+      spotifyApi.setAccessToken(params.access_token);
+    }
+
+    setloggedIn(params.access_token ? true : false);
+  }, [params.access_token]);
+  return loggedIn;
+}
+const useGetMe = () => {
+  const loggedIn = useAccessToken();
+  const [me, setme] = useState({});
+  useEffect(() => {
+    if (loggedIn) {
+      spotifyApi.getMe().then(user => {
+        console.log(user);
+        setme(user);
+      });
+    }
+  }, [loggedIn]);
+  return [me];
+};
+const useGetNowPlaying = () => {
+  const loggedIn = useAccessToken();
+
+  const [nowPlaying, setnowPlaying] = useState({
+    name: "",
+    albumArt: "",
+    is_playing: false,
+    uri: "",
+    id: ""
+  });
+  const [current, setcurrent] = useState(false);
+
+  useEffect(() => {
+    getCurrent();
+    const interval = setInterval(() => loggedIn && getCurrent(), 5000);
+    function getCurrent() {
+      spotifyApi.getMyCurrentPlaybackState().then(response => {
+        setcurrent(response ? response.is_playing : false);
+        if (response)
+          setnowPlaying({
+            name: response.item.name,
+            albumArt: response.item.album.images[0].url,
+            is_playing: response.is_playing,
+            uri: response.item.uri,
+            id: response.item.id
+          });
+      });
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [loggedIn]);
+
+  return [nowPlaying, current];
+};
+
+const useGetDevice = nowPlaying => {
+  const [devices, setdevices] = useState([]);
+  useEffect(() => {
+    if (nowPlaying.id) {
+      spotifyApi.getMyDevices().then(data => setdevices(data.devices));
+    }
+  }, [nowPlaying.id]);
+  return [devices];
+};
+
+const useRecomendation = nowPlaying => {
+  const [recomendation, setrecomendation] = useState([]);
+  useEffect(() => {
+    if (nowPlaying.id)
+      spotifyApi
+        .getRecommendations({
+          limit: 4,
+          market: "PE",
+          //seed_artists: "4NHQUGzhtTLFvgF5SZesLK",
+          seed_tracks: nowPlaying.id
+          //min_energy: 0.4,
+          //min_popularity: 90
+        })
+        .then(data => setrecomendation(data.tracks));
+  }, [nowPlaying.id]);
+  return [recomendation];
+};
+const useGetAudio = nowPlaying => {
+  const [audiodetail, setaudiodetail] = useState({});
+  useEffect(() => {
+    if (nowPlaying.id) {
+      spotifyApi
+        .getAudioFeaturesForTrack(nowPlaying.id)
+        .then(audiodetail => setaudiodetail(audiodetail));
+    }
+  }, [nowPlaying.id]);
+  return [audiodetail];
+};
+/*
+const getSearch = () => {
+  spotifyApi.search("WONDERLAND", ["track"], { market: "PE" }).then(device => {
+    console.log(device);
+  });
+};
+const createPlaylist = () => {
+  spotifyApi
+    .createPlaylist(me.id, {
+      name: "Creado desde el server",
+      public: false,
+      description: "esta lpaylist es la recomendada"
+    })
+    .then(device => {
+      console.log(device);
+    });
+};
+const addToPlaylist = () => {
+  if (nowPlaying.uri)
+    spotifyApi
+      .addTracksToPlaylist("06sL90oZx5lI8mbaGzlgFD", [nowPlaying.uri])
+      .then(device => {
+        console.log(device);
+      });
+};
+const removeToPlaylist = () => {
+  if (nowPlaying.uri)
+    spotifyApi
+      .removeTracksFromPlaylist("06sL90oZx5lI8mbaGzlgFD", [nowPlaying.uri])
+      .then(device => {
+        console.log(device);
+      });
+};
+*/
+const getHashParams = () => {
+  var hashParams = {};
+  var e,
+    r = /([^&;=]+)=?([^&;]*)/g,
+    q = window.location.hash.substring(1);
+  e = r.exec(q);
+  while (e) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
+    e = r.exec(q);
+  }
+  return hashParams;
+};
+export {
+  useAccessToken,
+  useGetMe,
+  useGetNowPlaying,
+  useGetDevice,
+  useRecomendation,
+  useGetAudio
+  /*,
+  getSearch,
+  createPlaylist,
+  addToPlaylist,
+  removeToPlaylist
+  */
+};
+/*
+
+
+*/
