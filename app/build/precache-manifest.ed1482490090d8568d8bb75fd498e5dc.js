38799df61a57826faf21f4c008434d9a245a1a53
Sergiolm17 <kingsergiolm17@gmail.com>
2019-09-21 04:10:33 +0000

    build

---
+++ app/build/precache-manifest.ed1482490090d8568d8bb75fd498e5dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b173540d01edc9619befbbc29208ce3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "346bea1ad8fe5174ebab",
+    "url": "/static/css/main.25baf33b.chunk.css"
+  },
+  {
+    "revision": "6e6451d4694fbde2951e",
+    "url": "/static/js/2.ab767f94.chunk.js"
+  },
+  {
+    "revision": "346bea1ad8fe5174ebab",
+    "url": "/static/js/main.d782934e.chunk.js"
+  },
+  {
+    "revision": "1246f5376b78f8c5fbeb",
+    "url": "/static/js/runtime~main.804b5e3b.js"
+  },
+  {
+    "revision": "554d941ca35261b58f0506e5fcc328fd",
+    "url": "/static/media/DOMO.554d941c.svg"
+  },
+  {
+    "revision": "d51ce46152e3b4983df9853426f7eb71",
+    "url": "/static/media/Happy.d51ce461.svg"
+  },
+  {
+    "revision": "75f61d59ab2ee0fcf04ee89889498d0e",
+    "url": "/static/media/Logo.75f61d59.svg"
+  },
+  {
+    "revision": "28aa50870e5adde80a756673306cb4ae",
+    "url": "/static/media/Sad.28aa5087.svg"
+  }
+]);
