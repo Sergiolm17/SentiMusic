47b743857f05bcafeb8539e102c79e9fb48714d2
Sergiolm17 <kingsergiolm17@gmail.com>
2019-09-23 17:46:49 +0000

    firebase

--- app/src/services/firebase_service.js
+++ app/src/services/firebase_service.js
@@ -1,1 +1,24 @@
 import firebase from "../Firebase";
+
+function PostAccion(user, accion) {
+  firebase
+    .firestore()
+    .collection("actions")
+    .add({
+      user,
+      accion,
+      date: new Date()
+    });
+}
+function PostTransition(user, from, to) {
+  firebase
+    .firestore()
+    .collection("transition")
+    .add({
+      user,
+      from,
+      to,
+      date: new Date()
+    });
+}
+export { PostAccion, PostTransition };
